fa6ccbea51607e830c2923ed2d8c08b9fc947da5
Vidal-Gonzalo <vidal.gonza23@gmail.com>
2023-04-04 16:38:37 -0300

    Change in hero icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Header from './components/Header'
 import About from './components/About'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
-import Footer from './components/Footer'
 
 const livvic = Livvic({
   subsets: ['latin'],
