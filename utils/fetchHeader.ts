afbe3388b1ec8b4855d99fe3aa0d138997f59afa
Vidal-Gonzalo <vidal.gonza23@gmail.com>
2023-04-16 21:06:51 -0300

    Change projectId property from sanity config

--- utils/fetchHeader.ts
+++ utils/fetchHeader.ts
@@ -1,11 +1,12 @@
 import { HeaderInfo } from "@/typings";
 
+const apiUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+
 export const fetchHeader = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getHeader`)
+    const res = await fetch(`${apiUrl}/api/getHeader`)
 
     const data = await res.json()
     const header: HeaderInfo = data.header;
-
 
     return header;
 }
