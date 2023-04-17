d705156c4daa37283a7d81e7654dfa37a7d13bd6
Vidal-Gonzalo <vidal.gonza23@gmail.com>
2023-04-16 22:11:40 -0300

    Changed project id in sanity config

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,8 @@ import {createClient} from 'next-sanity'
 import createImageBuilder from '@sanity/image-url'
 
 export const config = {
+  projectId: "2vuu1b7g",
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}`,
   apiVersion: "2022-02-03",
   useCdn: process.env.NODE_ENV === "production"
 }
